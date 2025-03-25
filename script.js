c1bd634581c54dfcabd28eea09db8e0475726036
StefanyLino <stefanylinosilva47@gmail.com>
2025-03-25 13:31:30 -0300

    SCRIPT - JS

--- script.js
+++ script.js
@@ -103,3 +103,51 @@ function animateText(){
 // Inicia a animação quando carregar a página
 document.addEventListener('DOMContentLoaded', animateText);
 updateTextColor();
+
+// ANIMAÇÃO DA SEÇÃO HOME 
+// Seleciona a seção home e aplica uma animação de fade-in
+const homeSection = document.querySelector('#home');
+homeSection.style.opacity = '0';
+homeSection.style.transform = 'translateY(200px)';
+homeSection.style.transition = 'opacity 1s ease, transform 1s ease';
+
+setTimeout(() => {
+  homeSection.style.opacity = '1';
+  homeSection.style.transform = 'translateY(0)';
+}, 100);
+
+// ANIMAÇÃO DAS SEÇÕES
+// Seleciona as animações e aplica fade-in ao atualizar
+const sections = document.querySelectorAll ('section');
+
+sections.forEach((section, index) => {
+    section.style.opacity = '0';
+    section.style.transition = 'opacity 1s, transform 1s';
+
+    // Aplica diferente transformações de acordo com  o índice da seção
+    if (index !== 0){
+        if(index === 1) section.style.transform = 'translateY(100px)';
+        else if (index === 2) section.style.transform = 'scale(0.8)';
+        else if (index === 3) section.style.transform = 'rotateY(90deg)';
+    }
+});
+
+// Observer para animar as seções ao rolar a página
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.style.opacity = '1';
+            entry.target.style.transform = 'none';
+        }
+    });
+});
+
+// Observa cada seção para aplicar a animação
+sections.forEach((section) => observer.observe(section));
+
+// BOTÃO DE VOLTAR AO TOPO
+// Adiciona um evento de clique ao botão de voltar ao topo
+document.querySelector('.top a').addEventListener('click', (e) => {
+    e.preventDefault();
+    window.scrollTo({ top: 0, behavior: 'smooth'}); // Rola suavemmente o topo da página
+})
